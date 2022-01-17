ea86d8d87bcdb2acc50f95b8e36b0de695f66837
github username <altmanguitar@gmail.com>
2022-01-16 23:38:33 -0500

    commit tree c60252251dd506683a8c6c4edd8c9b823927d674

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,50 @@
-function changeState(state, action){
+let state = { count: 0 };
+
+function changeState(state, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
     default:
       return state;
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function dispatch(action) {
+  state = changeState(state, action);
+  return state;
+}
 
-changeState(state, action)
+function render() {
+  document.body.textContent = state.count;
+}
+
+// dispatch({ type: "INCREASE_COUNT" });
+// dispatch({ type: "INCREASE_COUNT" });
+
+// call the render function
+render();
+
+
+
+
+
+// function changeState(state, action){
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     default:
+//       return state;
+//   }
+// }
+
+// function dispatch(action) {
+//   state = changeState(state, action);
+//   return state;
+// }
+
+// let state = {count: 0}
+// let action = {type: 'INCREASE_COUNT'}
+
+// changeState(state, action)
+
+// dispatch(action)
